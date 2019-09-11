f630c0d75d6b422a87b7f20cb302b48deece489b
simonkarlsen <si-karlsen@hotmail.com>
2019-09-11 13:53:33 +0200

    maven dependency

---
+++ src/test/java/no/kristiania/HTTPServer/HTTPClient.java
@@ -0,0 +1,14 @@
+package no.kristiania.HTTPServer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HTTPClient {
+
+    @Test
+    void mathShouldWork() {
+        assertEquals(4, 2+2);
+    }
+
+}
